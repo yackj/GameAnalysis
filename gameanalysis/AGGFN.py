768ea19d88fb270143b6238401c023b7c0baabc3
Fengjun Yang <fyang1@swarthmore.edu>
2016-05-31 12:19:15 -0400

    can now be called from nash.mixed_nash

--- gameanalysis/AGGFN.py
+++ gameanalysis/AGGFN.py
@@ -19,17 +19,14 @@ class Sym_AGG_FNA(rsgame.EmptyGame):
             action_graph: {strategy: [neighbor]}
             utilities: {strategy: np.array[weight]}
                     For each strategy, store the weight of all its neighbors
-            functions: {function node: tuple(type of function,(param))}
+            functions: {function node: list(type of function,(param))}
         Class Variables:
             self.neighbors: {node: {neighbor: neighbor's local index}}
             self.neighbor_index: {node: [if a node is in this neighborhood]}
             self._dev_reps: np.array[(n-1)C(0),(n-1)C(1),...,(n-1)C(n-1)]
-            self.func_dict: {type of function: its generator}
             self.func_table: store the tabular format of functions
         """
         super().__init__({"All":players}, {"All":set(strategies)})
-        self.players = players
-        self.strategies = strategies
         self.function_nodes = function_nodes
         self.utilities = utilities
         self.functions = functions
@@ -38,18 +35,32 @@ class Sym_AGG_FNA(rsgame.EmptyGame):
                              for i,n in enumerate(sorted(action_graph[s]))} \
                                         for s in self.nodes}
         self.neighbor_index = {s:np.array([n in self.neighbors[s] \
-                                            for n in self.strategies]) \
+                                            for n in self.strategies['All']]) \
                                             for s in self.nodes}
         self.dev_reps = np.array([np.log(comb(players-1,i)) \
                                         for i in range(players)])
-        self.func_dict = {
-            'quadratic': lambda x,y,z: \
-                            [x*i**2 + y*i + z for i in range(players+1)],
-            'linear': lambda k,c: [k*x + c for x in range(players+1)]
-        }
 
-        self.func_table = {p: self.func_dict[self.functions[p][0]](
-                        *(self.functions[p][1])) for p in self.function_nodes}
+        self.func_table = {p: self.build_func_table(self.functions[p],players) \
+                                for p in self.function_nodes}
+
+    def build_func_table(self,func,N):
+        """
+        Input:
+          function: a [func_type, parameters] list
+          N: number of players
+        Output:
+          function values represented as a list
+        """
+        func_type = func[0]
+        param = func[1]
+        if func_type == 'linear':
+            k, c = param
+            return [k*n + c for n in range(N+1)]
+        elif func_type == 'quadratic':
+            a,b,c = param
+            return [a*n**2 + b*n + c for n in range(N+1)]
+        else:
+            raise KeyError("No such function type")
 
     @staticmethod
     def from_json(json_):
@@ -74,11 +85,11 @@ class Sym_AGG_FNA(rsgame.EmptyGame):
             if node in self.function_nodes:
                 d[node] = (np.min(self.func_table[node]),
                            np.max(self.func_table[node]))
-            elif node in self.strategies:
-                d[node] = (0, self.players)
+            elif node in self.strategies['All']:
+                d[node] = (0, self.players['All'])
 
         min_payoff = float('inf')
-        for s in self.strategies:
+        for s in self.strategies['All']:
             min_config = [d[n][0] \
                             if self.utilities[s][self.neighbors[s][n]] >= 0 \
                             else d[n][1] for n in self.neighbors[s]]
@@ -87,7 +98,7 @@ class Sym_AGG_FNA(rsgame.EmptyGame):
                 min_payoff = min_s
 
         if as_array or as_array is None:
-            return [min_payoff]
+            return np.array([min_payoff])
         else:
             return {'All': min_payoff}
 
@@ -101,13 +112,13 @@ class Sym_AGG_FNA(rsgame.EmptyGame):
         """
         mix = self.as_mixture(mix, verify=verify, as_array=True)
         EVs = []
-        for strat in self.strategies:
+        for strat in self.strategies['All']:
             # local_mix: if is action node append the prob 
             #            if function sum its neighbors
             local_mix = np.zeros(len(self.neighbors[strat]),float)
             for s,i in self.neighbors[strat].items():
-                if s in self.strategies:
-                    local_mix[i] = mix[self.strategies.index(s)]
+                if s in self.strategies['All']:
+                    local_mix[i] = mix[self.strategies['All'].index(s)]
                 if s in self.function_nodes:
                     local_mix[i] = sum(mix[self.neighbor_index[s]])
             local_mix += tiny # prevent log(0)
@@ -119,11 +130,11 @@ class Sym_AGG_FNA(rsgame.EmptyGame):
                 prob = local_mix[self.neighbors[strat][s]]
 
                 sigma = np.array([(np.log(prob) * i + \
-                                  np.log(1-prob+tiny) * (self.players-i-1)) \
-                                  for i in range(self.players)])
+                                  np.log(1-prob+tiny) * (self.players['All']-i-1)) \
+                                  for i in range(self.players['All'])])
 
-                if s in self.strategies:
-                    f = np.array(range(self.players))
+                if s in self.strategies['All']:
+                    f = np.array(range(self.players['All']))
                     if s == strat:
                         f += 1
                 elif s in self.function_nodes:
@@ -134,15 +145,16 @@ class Sym_AGG_FNA(rsgame.EmptyGame):
 
                 EV[i] = np.sum(f * np.exp(self.dev_reps + sigma))
             EVs.append(np.dot(EV,self.utilities[strat]))
-        return EVs
+
+        return np.array(EVs)
 
     def to_json(self):
         """
         Creates a json format of the game for storage
         """
         json = {}
-        json['players'] = self.players
-        json['strategies'] = self.strategies
+        json['players'] = self.players['All']
+        json['strategies'] = list(self.strategies['All'])
         json['function_nodes'] = self.function_nodes
         action_graph = {s:list(self.neighbors[s].keys()) \
                 for s in self.neighbors}
@@ -163,7 +175,7 @@ class Sym_AGG_FNA(rsgame.EmptyGame):
         if D_min < 0 or D_min >= num_strats:
             D_min = 0
         if D_max < 0 or D_max >= num_strats:
-            D_max = S-1
+            D_max = num_strats / 2
 
         # This maps a function to the (mean, var) tuple for params
         func = {
